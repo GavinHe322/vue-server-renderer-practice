17c5becddea9cfcdeeaccd7ca5b36287fbd38287
GavinHe322 <https://github.com/GavinHe322>
2022-01-13 23:58:23 +0800

    feat: ssr 项目结构

---
+++ example/demo-4/entry-client.js
@@ -0,0 +1,9 @@
+// 仅运行于浏览器
+
+import { createApp } from './app'
+
+// 客户端逻辑...
+
+const { app } = createApp()
+
+app.$mount('#app')
---
+++ example/demo-4/entry-server.js
@@ -0,0 +1,8 @@
+// 仅运行于服务器
+
+import { createApp } from './app'
+
+export default context => {
+  const { app } = createApp()
+  return app
+}
